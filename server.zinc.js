771dc636a456baad6324f6d58d8403b6b460a17c
Dilean Barroso <dileanbarroso@gmail.com>
2017-09-21 17:22:53 -0400

    app to zinc

--- server.zinc.js
+++ server.zinc.js
@@ -2,34 +2,34 @@ const express = require('express'), path = require('path'), favicon = require('s
 const cookieParser = require('cookie-parser'), bodyParser = require('body-parser'), mongoose = require('mongoose');
 const api = require('./controllers/api.zinc')
 
-const app = express();
+const zinc = express();
 const log = require('./controllers/logger.zinc')
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+zinc.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+zinc.use(logger('dev'));
+zinc.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }));
+zinc.use(cookieParser());
+zinc.use(express.static(path.join(__dirname, 'public')));
 
 // Api Starts Here
-app.use('/api', api, function (req, res, next) {
+zinc.use('/api', api, function (req, res, next) {
   res.send(err)
 })
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+zinc.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 // error handler
-app.use(function (err, req, res, next) {
+zinc.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.zinc.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+module.exports = zinc;
